e09f7bcba1a7e008bcd7693fb6584fdbbc8c0c72
Peter Phelan <pphelan69@gmail.com>
2016-11-18 14:25:14 -0800

    added stub for PasswordTest

---
+++ src/test/java/com/servicenow/gurukula/PasswordTest.java
@@ -0,0 +1,24 @@
+package com.servicenow.gurukula;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by pphelan on 11/18/2016.
+ */
+public class PasswordTest {
+
+    private WebDriver driver;
+    private String baseUrl;
+
+    /*
+    <todo> Add Tests:
+    - Check password length check errors message.
+    - Mismatched password get error message
+    - When 'New' and 'New password confirmation' textfields match the 'Save' button becomes active and can click.
+    - Change password and see if you can log in with that new password
+    - Check password can not be longer than 50 characters.
+    */
+    
+
+
+}
